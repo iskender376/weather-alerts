7142b6d33064b37ffc28044ddbaafede61b74ab7
Iskender <isgender376@gmail.com>
2025-02-14 15:47:31 +0400

    wip: Added API routes for weather alerts

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\WeatherAlertController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/alerts', [WeatherAlertController::class, 'index']);
+    Route::post('/alerts', [WeatherAlertController::class, 'store']);
+    Route::delete('/alerts/{alert}', [WeatherAlertController::class, 'destroy']);
+});
