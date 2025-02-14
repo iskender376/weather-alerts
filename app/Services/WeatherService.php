5a811837a85a7d35a76989a521da186ce7ad1d96
Iskender <isgender376@gmail.com>
2025-02-14 16:14:41 +0400

    wip: Manually created WeatherService for fetching weather data

---
+++ app/Services/WeatherService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class WeatherService
+{
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->apiKey = env('OPENWEATHER_API_KEY');
+    }
+
+    public function getWeather($city)
+    {
+        $response = Http::get("https://api.openweathermap.org/data/2.5/weather", [
+            'q' => $city,
+            'appid' => $this->apiKey,
+            'units' => 'metric'
+        ]);
+
+        return $response->json();
+    }
+}
