c838a46b2afacda85cfbaf1f1888f0003b608dc5
Iskender <isgender376@gmail.com>
2025-02-15 18:28:16 +0400

    wip: Created WeatherPushNotification for WebPush alerts

---
+++ app/Notifications/WeatherPushNotification.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WeatherPushNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function via($notifiable)
+    {
+        return [WebPushChannel::class];
+    }
+
+
+    public function toWebPush($notifiable, $notification)
+    {
+        return (new WebPushMessage)
+            ->title('Weather Alert!')
+            ->body('High precipitation or UV index detected!')
+            ->action('View Details', 'view_alert')
+            ->data(['id' => $notification->id]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
