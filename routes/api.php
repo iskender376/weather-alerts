aeba1743c916a10454dc6aff5ac712d90ccb56bb
Iskender <isgender376@gmail.com>
2025-02-15 18:29:52 +0400

    wip: Added WebPush subscription API routes

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,19 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/alerts', [WeatherAlertController::class, 'store']);
     Route::delete('/alerts/{alert}', [WeatherAlertController::class, 'destroy']);
     Route::get('/weather/{city}', [WeatherAlertController::class, 'getWeatherData']);
+    Route::post('/push/subscribe', function (Request $request) {
+        $request->user()->updatePushSubscription(
+            $request->endpoint,
+            $request->keys['p256dh'],
+            $request->keys['auth']
+        );
+
+        return response()->json(['message' => 'Subscribed successfully']);
+    });
+
+    Route::post('/push/unsubscribe', function (Request $request) {
+        $request->user()->deletePushSubscription($request->endpoint);
+
+        return response()->json(['message' => 'Unsubscribed successfully']);
+    });
 });
